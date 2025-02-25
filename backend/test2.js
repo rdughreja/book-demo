683251bb609f5703be064cdc5575e8bb1dbbdd70
rdughreja <132198083+rdughreja@users.noreply.github.com>
2025-02-25 12:46:52 +0530

    test2.

--- backend/test2.js
+++ backend/test2.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { MongoClient, ObjectId } = require('mongodb');
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 const uri = 'mongodb+srv://KrinaBhalodiya:krina22030401020@cluster0.lmttp.mongodb.net/';
 const client = new MongoClient(uri);
